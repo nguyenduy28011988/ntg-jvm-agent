30ba2dff0ace07562386c42b7835d3d55e410115
Duy Nguyen <Duy.Nguyen@nashtechglobal.com>
2025-09-30 12:08:51 +0700

    update

--- orchestrator/src/main/kotlin/com/ntgjvmagent/orchestrator/OrchestratorApplication.kt
+++ orchestrator/src/main/kotlin/com/ntgjvmagent/orchestrator/OrchestratorApplication.kt
@@ -6,7 +6,6 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class OrchestratorApplication
 
-
 fun main(args: Array<String>) {
     runApplication<OrchestratorApplication>(*args)
 }
