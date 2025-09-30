7b08f0986615594d63171f1ef2486c64c2893393
Duy Nguyen <Duy.Nguyen@nashtechglobal.com>
2025-09-30 15:03:39 +0700

    test ci

--- chat-ui/src/middleware.ts
+++ chat-ui/src/middleware.ts
@@ -6,7 +6,7 @@ export function middleware(req: NextRequest) {
   const isAuthPage =
     req.nextUrl.pathname.startsWith('/login') || req.nextUrl.pathname.startsWith('/auth');
 
-  // If not login yet and not in login page → redirect to /login
+  // If not login yet and not in login page → redirect to /login.
   if (!accessToken && !isAuthPage) {
     return NextResponse.redirect(new URL('/login', req.url));
   }
